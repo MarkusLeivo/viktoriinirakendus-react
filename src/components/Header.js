884d4778620ffdfde10c00a306098c8a5b07ccfc
MarkusLeivo <markusleivo20@gmail.com>
2026-03-13 12:11:14 +0200

    Logo fix

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ function Header() {
     <header className="navbar">
         <div className="meta-container"></div>
         <div className="nav-container">
-            <img src={`${process.env.PUBLIC_URL}/ESlogo.SVG`} alt="logo" className="logo" />
+            <img src={`${process.env.PUBLIC_URL}/ESlogo.svg`} alt="logo" className="logo" />
         </div>
     </header>
   );
